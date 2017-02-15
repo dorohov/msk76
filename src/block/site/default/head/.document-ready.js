22095c4eb73bea7bedaf24b58a13329c065bd38f
caezar47 <iss47@ya.ru>
2017-02-15 11:29:24 +0300

    optimiz

--- src/block/site/default/head/.document-ready.js
+++ src/block/site/default/head/.document-ready.js
@@ -20,62 +20,9 @@ $('.btn-journal').on('click', function(){
   	$(this).toggleClass('is-active');
   	$("._dipc__journal-list").toggleClass('is-visible');  
 });
-$("form.form-site").validationEngine(
-	'attach', {
-		promptPosition : "bottomLeft"
-	}
-);
 retinajs();
-/*
-$('.navbar-project a[href="'+url+'"]').parent().addClass('active'); 
-$('.tabs-block a[href="'+url+'"]').parent().addClass('active'); 
-$('._ipc__carousel .item').eq(0).addClass('active');
-$('._adpc__carousel .item').eq(0).addClass('active');
-$('._ipc__carousel').carousel({
-    interval : false
-});*/
-/*$('.fancybox').fancybox({
-	prevEffect : 'none',
-	nextEffect : 'none',
-});*/
-// конструкция для активации fancybox <a class="fancybox-buttons" data-fancybox-group="button" href="" data-title=""><img src="" alt="" /></a>
-/*$('.fancybox-buttons').fancybox({
-	openEffect  : 'none',
-	closeEffect : 'none',
-	prevEffect : 'none',
-	nextEffect : 'none',
-	closeBtn  : false,
-	helpers : {
-		title : {
-			type : 'inside'
-		},
-		buttons	: {}
-	},
-	afterLoad : function() {
-		this.title = this.title;
-	}
-});*/
 
 /*
-$('._ipc__list').owlCarousel({
-	margin: 20,
-	items:8,	
-	navText: [],
-	//autoplay:true,
-	//smartSpeed: 500,
-	//autoplayTimeout:10000,
-	nav: false,
-	dots: false,
-	responsive: {
-		0:{
-			items:3,
-			dots: true,
-		},
-		767:{
-			items:8,
-		}
-	}
-});	
 $('.getModal').click(function(event){
 	event.preventDefault();
 	
--- src/block/site/default/head/.window-resize.js
+++ src/block/site/default/head/.window-resize.js
@@ -14,55 +14,16 @@ var h_window = $(window).height(),
 	h_team = h_window - h_footer, 
 	h_content = h_window - h_navbar - h_footer,
 	h_content_xs = h_window - h_navbar;
-
-if (!device.mobile() || !device.tablet()) {
-	$('.content-block.second').css("min-height", h_content);	
-	//$('._dipc__journal-list').css("height", h_journal_list);	
-}
-
-if (device.tablet()) {
-	$//("._msp__block .scroller").mCustomScrollbar();
-	//$('.index-page-content').css("min-height", h_content_index);	
-	//$('.content-block.second').css("min-height", h_content_scroller_sm);
-	//$('._adpc__row').css("min-height", h_content_scroller_sm);
-	//$('._gpc__preview-cols').css("max-width", w_window);
-}
+	
 if (device.mobile()) {	
 	$('.layouts-page-content').css("min-height", h_content_xs);
 } else{
 	$('.twoGIS-map__block').css("height", h_map);
-	/*$('._adpc__carousel').carousel({
-	    interval : false
-	});	*/
-	//$('.about-team-page-content').css("height", h_team);
 }
 if (device.mobile() || device.tablet()) {
 	$("._iabai__cols.one").prependTo($("._iabai__row._two"));
 	$("._ifbc__btn-block").appendTo($("._ifb__complex"));
 	$(".bg-element").remove();
-	//$('.second-page .navbar-nav li a').removeAttr("data-menu-top");	
-
-	//$('.navbar').addClass('navbar-fixed-top');
-	//$('.navbar').css("max-width", w_window);
-	/*$('.owl-slider ul').owlCarousel({
-		margin: 0,
-		loop: true,
-		items:1,	
-		navText: [],
-		//autoplay:true,
-		smartSpeed: 500,
-		autoplayTimeout:10000,
-		nav: true,
-		dots: false		
-	});	*/
-	//$("._ilb__items-block").appendTo($(".index-locaion-block"));
 } else {
-	//$('.content-block.second').css("min-height", h_content_scroller);
-	//$('.contacts-page-content').css("height", h_content_scroller);
-	//$('.content-block.second').css("top", h_header);
-	//$(".scroller").mCustomScrollbar();	
-	//$("._atpc__heading").prependTo($("._atpc__tab-pane-cols-right"));		
-	//$(".error-page-content").css("height", h_content_scroller);	
+	$('.content-block.second').css("min-height", h_content);
 }
-//$(".navbar-fixed-top").autoHidingNavbar();
-//$('.news-item__preview a').css("height", w_news_preview);
