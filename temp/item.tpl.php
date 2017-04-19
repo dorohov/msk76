4765b51315d3a2f91d5bfa0fd04e0223a23401bb
caezar47 <iss47@ya.ru>
2017-04-19 16:22:19 +0300

    add banner

--- temp/item.tpl.php
+++ temp/item.tpl.php
@@ -1,3 +1,17 @@
 <?
 
-the_content();
+
+
+?>
+<div class="second-page-container">
+	<div class="content-block second text-page-content">
+		<div class="container _tcp__container container-content">	
+			<div class="text-container">
+				<h1 class="heading-site"><? the_title();?></h1>			
+				<div class="text-block">
+					<? the_content();?>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
